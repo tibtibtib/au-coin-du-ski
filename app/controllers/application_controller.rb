c7e906262bf87ca2a098df2d4ba7eebcca06a922
Gaetan Mallet <mallet.gaetan@hotmail.fr>
2020-03-10 10:34:53 +0100

    end with avatar for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :cell_phone, :profile_picture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :cell_phone, :avatar])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :cell_phone, :profile_picture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :cell_phone, :avatar])
   end
 end
