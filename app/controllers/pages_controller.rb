1c7848fd943fc84fb1dd2db4f90959508a1004b0
Alix Gauthier <gauthieralix@gmail.com>
2020-03-10 12:41:09 +0100

    change navbar style add listings on home add concept myproducts mybookings pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
   end
+  def concept
+  end 
+  def my_bookings
+  	
+  end
+  def my_products
+  	
+  end 
 end
