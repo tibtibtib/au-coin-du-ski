e99e60fade5baba67ed70e0be99ade2cb87cdd64
Gaetan Mallet <mallet.gaetan@hotmail.fr>
2020-03-12 16:11:48 +0100

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,8 @@ import "bootstrap";
 
 import { fixFooter } from '../plugin/footer';
 import { onScrollNavBar } from '../plugin/navbar';
-<<<<<<< HEAD
 import { filtersProducts } from '../plugin/productFilter';
-=======
 import { updatePriceOnDateClick } from '../plugin/calculate_price';
->>>>>>> 723a2cc4668f82249157311155d1de7ad5831e25
 
 fixFooter();
 
