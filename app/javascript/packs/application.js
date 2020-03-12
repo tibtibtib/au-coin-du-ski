574eea57035cf752548ae87bf0a7cc1cbecbec94
Jerome Crest <jerome.crest@gmail.com>
2020-03-12 15:42:17 +0100

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 
 import { fixFooter } from '../plugin/footer';
 import { onScrollNavBar } from '../plugin/navbar';
-import { } from '../plugin/calculate_price';
+import { updatePriceOnDateClick } from '../plugin/calculate_price';
 
 fixFooter();
 
