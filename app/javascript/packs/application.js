e5d7daebed111f57849c6bdcd49f04768fe4a6ac
Alix Gauthier <gauthieralix@gmail.com>
2020-03-12 17:03:49 +0100

    under construction

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,5 @@
 import "bootstrap";
 import { initStarRating } from '../plugins/init_star_rating';
-
-initStarRating();
-
 import { fixFooter } from '../plugin/footer';
 import { onScrollNavBar } from '../plugin/navbar';
 import { updatePriceOnDateClick } from '../plugin/calculate_price';
@@ -11,3 +8,4 @@ fixFooter();
 
 onScrollNavBar();
 
+initStarRating();
