9a5c5a7f16b0c3393fafbe32df5819f9ff60b1d8
Jerome Crest <jerome.crest@gmail.com>
2020-03-12 12:14:39 +0100

    initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 
 import { fixFooter } from '../plugin/footer';
 import { onScrollNavBar } from '../plugin/navbar';
+import { } from '../plugin/calculate_price';
 
 fixFooter();
 
