982b61cdbee5e827afeb46cccb92938c279ae53b
Alix Gauthier <gauthieralix@gmail.com>
2020-03-12 15:39:56 +0100

    work under construction

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
+import { initStarRating } from '../plugins/init_star_rating';
+
+initStarRating();
 
 import { fixFooter } from '../plugin/footer';
 import { onScrollNavBar } from '../plugin/navbar';
