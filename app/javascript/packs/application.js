ceac769da0477b92e7cf1d62463044004bd7345f
Thibault Adet <thibault.adet@gmail.com>
2020-03-12 15:32:07 +0100

    wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,11 @@ import "bootstrap";
 
 import { fixFooter } from '../plugin/footer';
 import { onScrollNavBar } from '../plugin/navbar';
+import { datePicker } from '../plugin/datepicker';
 
 fixFooter();
 
 onScrollNavBar();
+
+datePicker();
 
---
+++ app/javascript/plugin/datepickr.js
@@ -0,0 +1,9 @@
+import flatpickr from 'flatpickr';
+import "flatpickr/dist/flatpickr.min.css"
+
+const datePicker = () => {
+  flatpickr(".datepicker", {});
+};
+
+
+export { datePicker };
